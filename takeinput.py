2fade07d295cf4e1b132d86b460f53cf1dd3b7e1
Maha <Maha@SAADs-MacBook-Pro.local>
2026-05-22 04:21:48 +0500

    autosave 2026-05-22 04:21:48

--- takeinput.py
+++ takeinput.py
@@ -3,3 +3,9 @@ b= input('enter bhi: ')
 
 print('tumne likha:  ', a)
 print('tumne likha:  ', b)
+
+#print('sum of iskay answers ', a+b) #concatination horhi coos string
+#int banado
+e= int(input('enter kuch: '))
+f= int(input('enter bhi: ')) # test
+print('sum of iskay answers ', e+f)
