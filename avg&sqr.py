23a851823c5793ef0d4fac9933ba16766869c32e
Maha <Maha@SAADs-MacBook-Pro.local>
2026-05-27 03:12:51 +0500

    autosave 2026-05-27 03:12:51

---
+++ avg&sqr.py
@@ -0,0 +1,11 @@
+#find avg of 2 number by users
+
+a=int(input('enter num1: '))
+b=int(input('enter num2: '))
+
+print((a+b)/2)
+
+#find square of num by user
+z=int(input('enter num1: '))
+
+print(z*z)
---
+++ prac2remainder.py
@@ -0,0 +1,6 @@
+#find remainder when number is divisible by z
+
+a= int(input('write any number'))
+b= int(input('write any number'))
+remainder = a % b
+print("Remainder is:", remainder)
